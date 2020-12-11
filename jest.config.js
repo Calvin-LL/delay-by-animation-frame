0b772245363ebdb5d7b6cb2efee1506733af9cf1
Calvin Liang <me@calvin.sh>
2020-12-11 13:18:33 -0800

    test: remove coverage info

--- jest.config.js
+++ jest.config.js
@@ -8,14 +8,4 @@ module.exports = {
   testMatch: ["<rootDir>/test/**/*.test.ts"],
   testEnvironment: "node",
   testTimeout: 10000,
-  collectCoverage: true,
-  coveragePathIgnorePatterns: ["<rootDir>/test/", "<rootDir>/node_modules/"],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
 };
